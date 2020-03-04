dc30718461e7307c9e6e39c6b538e805a8647d80
Constantine Theocharis <kontheocharis@gmail.com>
2020-03-04 19:54:36 +0000

    Update FmpDataFetcher class to work with local files (untested)

--- alpha/alpha/fmp.py
+++ alpha/alpha/fmp.py
@@ -3,56 +3,74 @@ import requests
 import os
 import hashlib
 import json
-from typing import Tuple
+from typing import Tuple, Option
+from enum import Enum
 
 from alpha.base import DataFetcher
 
+
+class Statement(Enum):
+    BALANCE_SHEET = 0
+    INCOME_STATEMENT = 1
+    CASH_FLOW_STATEMENT = 2
+    SHARE_PRICES = 3
+
+    def to_string(self) -> str:
+        if self == Statement.BALANCE_SHEET:
+            return 'balance-sheet-statement'
+        elif self == Statement.INCOME_STATEMENT:
+            return 'income-statement'
+        elif self == Statement.CASH_FLOW_STATEMENT:
+            return 'cash-flow-statement'
+        elif self == Statement.SHARE_PRICES:
+            return 'share-prices'
+        assert False
+
+
 class FmpError(Exception):
     pass
+
 
 class FmpDataFetcher(DataFetcher):
     """
     An implementation of `DataFetcher` using financialmodelingprep.com API
 
     :param company_symbol: the stock market symbol of the company in question.
-
     :param year_range: a tuple in the form `(start_year, end_year)` for which
-    to fetch data
+        to fetch data
+    :param data_folder: the folder in which the data from financialmodelingprep
+        is located.
     """
 
     _year_start: int
     _year_end: int
     _symb: str
+    _data_folder: Option[str]
 
     _BASE_URL = 'https://financialmodelingprep.com/api/v3'
     _CACHE_LOCATION = os.path.join(os.path.dirname(__file__), '../cache/')
 
-    def __init__(self, company_symbol: str, year_range: Tuple[int, int]):
+    _FINANCIAL_COMP = "financials/{}/{}"
+    _SHARE_COMP = "historical-price-full/{}?from={}&to={}"
+
+    def __init__(self, company_symbol: str, year_range: Tuple[int, int], data_folder: Option[str] = None):
         self._year_start, self._year_end = year_range
         self._symb = company_symbol
-        pass
+        self._data_folder = data_folder
 
 
     def financial_data(self) -> pd.DataFrame:
-        statements = ('balance-sheet-statement', 'income-statement', 'cash-flow-statement')
-
-        urls = {statement: f"{self._BASE_URL}/financials/{statement}/{self._symb}" \
-                for statement in statements}
+        statements = (Statement.BALANCE_SHEET, Statement.INCOME_STATEMENT, Statement.CASH_FLOW_STATEMENT)
 
         raw_dfs = {}
         for statement in statements:
-            data = json.loads(self._load_resource(urls[statement]))
-            if not data: raise FmpError("no data was returned")
-
+            data = json.loads(self._load_resource(statement))
+            if not data: raise FmpError(f"no {statement.to_string()} data for {self._symb}")
             raw_dfs[statement] = pd.DataFrame(data['financials'])
-            df = raw_dfs[statement]
-            df.set_index('date', inplace=True)
-            df.index.name = None
-            df.index = pd.to_datetime(df.index)
 
-        ics = raw_dfs['income-statement']
-        bls = raw_dfs['balance-sheet-statement']
-        cfs = raw_dfs['cash-flow-statement']
+        ics = raw_dfs[Statement.INCOME_STATEMENT]
+        bls = raw_dfs[Statement.BALANCE_SHEET]
+        cfs = raw_dfs[Statement.CASH_FLOW_STATEMENT]
 
         mappings = {
             'total_outstanding_shares':    ics['Weighted Average Shs Out'],
@@ -67,32 +85,49 @@ class FmpDataFetcher(DataFetcher):
         }
 
         df = pd.DataFrame(mappings, copy=False)
+        df = self._format_df(df)
+        return df
+
+
+    def stock_data(self) -> pd.DataFrame:
+        data = json.loads(self._load_resource(Statement.SHARE_PRICES))
+        if not data: raise FmpError(f"no {statement.to_string()} data for {self._symb}")
+
+        df = pd.DataFrame(data['historical']).filter(items=('date', 'high', 'low'))
+        df = self._format_df(df)
+        return df
+
+
+    def _format_df(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Gets `df` in the right format, including parsing indices and values,
+        and limiting to applicable dates.
+        """
+
+        df.set_index('date', inplace=True)
+        df.index.name = None
+        df.index = pd.to_datetime(df.index)
         df = df.apply(pd.to_numeric, errors='raise')
         df = df.loc[str(self._year_end):str(self._year_start)]
         return df
 
 
-    def stock_data(self) -> pd.DataFrame:
-        url = f"{self._BASE_URL}/historical-price-full/{self._symb}?from={self._year_start}&to={self._year_end + 1}"
-        data = json.loads(self._load_resource(url))
-        if not data: raise FmpError("no data was returned")
-
-        df = pd.DataFrame(data['historical']).filter(items=('date', 'high', 'low'))
-        df.set_index('date', inplace=True)
-        df.index.name = None
-        df.index = pd.to_datetime(df.index)
-        df = df.apply(pd.to_numeric, errors='raise')
-        return df
-
-
-    def _load_resource(self, url: str) -> str:
+    def _load_resource(self, statement: Statement) -> str:
         """
         Loads a url resource.
 
-        Looks at the cache first (see `self._CACHE_LOCATION`) and returns the
-        resource if present.  Otherwise, fetches the resource at `url`, returns
-        it and updates the cache.
+        If `self._data_folder` exists, loads from there. Otherwise, looks at
+        the cache first (see `self._CACHE_LOCATION`) and returns the resource
+        if present.  If it isn't, fetches the resource at `url`, returns it and
+        updates the cache.
         """
+
+        if self._data_folder:
+            return json.loads(os.path.join(self._data_folder, statement.to_string(), f"{self._symb}.json"))
+
+        url = f"{self._BASE_URL}/{self._FINANCIAL_COMP.format(statement.to_string(), self._symb)}" \
+            if statement != Statement.SHARE_PRICES else \
+            f"{self._BASE_URL}/{self._SHARE_COMP.format(self._symb, self._year_start, self._year_end + 1)}"
 
         url_hash = hashlib.sha1(url.encode('utf-8')).hexdigest()[:10]
         file_location = os.path.join(self._CACHE_LOCATION, url_hash)
