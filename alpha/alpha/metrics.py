59e7e5e5ae7e42341160ab181b2b95901fd05176
Constantine Theocharis <kontheocharis@gmail.com>
2020-03-06 19:35:40 +0000

    Implement BacktrackingAnalyser to determine accuracy of method

--- alpha/alpha/metrics.py
+++ alpha/alpha/metrics.py
@@ -3,9 +3,10 @@ from abc import ABC, abstractmethod
 from typing import Dict, List
 from statistics import mean
 from dataclasses import dataclass
+from datetime import date
 
 import numpy as np
-
+import pandas as pd
 
 @dataclass(init=False)
 class Metrics(ABC):
@@ -25,17 +26,50 @@ class Metrics(ABC):
             print(f"\t{k} = {v}")
 
 
-class MetricAnalyser:
+# TODO: add documentation
+class BacktrackingAnalyser:
     """
     Container for all the metrics, for purposes of statistical analysis.
     """
-    metrics: Dict[str, Metrics]
+    _metric_data: Dict[str, Metrics]
+    _stock_data: Dict[str, pd.DataFrame]
+    _investing_prices: Dict[str, np.float64]
+
+    _investing_date: date
+    _return_percent: float
+
+
+    def __init__(self, investing_date: date, return_percent: float):
+        self._investing_date = investing_date
+        self._return_percent = return_percent
+
+
+    def add_metrics_for(company: str, metrics: Metrics):
+        self._metric_data[company] = metrics
+
+    def add_stock_df_for(company: str, stock_df: pd.DataFrame):
+        self._stock_data[company] = stock_df
+
+    def add_investing_price_for(company: str, price: np.float64):
+        self._investing_prices[company] = price
+
 
     def average_accuracy(self) -> float:
+        return mean(self._prediction_was_sucessful(self._stock_data[company], self._investing_prices[company]) \
+                    for company, metrics in self._metric_data.items() if metrics.are_investable())
+
+
+    def investable_percent(self) -> float:
         """
         Averages amount of values in `self.metrics` which are investable.
         """
-        return mean(m.are_investable() for m in self.metrics.values())
+        return mean(metrics.are_investable() for metrics in self._metric_data.values())
+
+
+    def _prediction_was_sucessful(stock_df: pd.DataFrame, investing_price: np,float64) -> bool:
+        relevant_df = stock_df[stock_df.index.date > self._investing_date]
+        max_price = stock_df.max()["high"]
+        return max_price >= _return_percent * investing_price
 
 
 # Actual metric definitions:
