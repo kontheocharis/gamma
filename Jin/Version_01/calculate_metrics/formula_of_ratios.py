0fc8990180e9190b562437043aa103486d310621
Jin Lee <jininvt@gmail.com>
2020-05-02 01:02:31 +0800

    Fix Python formatting based on PEP8.

--- Jin/Version_01/calculate_metrics/formula_of_ratios.py
+++ Jin/Version_01/calculate_metrics/formula_of_ratios.py
@@ -3,88 +3,85 @@ import numpy as np
 
 
 def debt_to_equity_ratio(shortterm_debt, longterm_debt, total_equity):
+    '''To calculate debt-to-equity ratio.'''
 
     # To check if arrays are of the same length. If not, return an error.
     if len(shortterm_debt) != len(longterm_debt) or len(longterm_debt) != len(total_equity):
         return 'Cant do calculation as arrays are of different lengths'
-    
-    total_debt = np.add(shortterm_debt, longterm_debt)
 
-    return np.divide( total_debt, total_equity, out=np.zeros_like(total_debt), where=total_equity!=0)
+    total_debt = shortterm_debt + longterm_debt
+
+    return np.divide(total_debt, total_equity, out=np.zeros_like(total_debt), where=total_equity != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
 
 
-
 def positive_operating_cashflow(operating_cashflow):
+    '''To get positive operating cashflow.'''
 
     # I will write program to check if operating cashflow is positive for 3 years later
 
     return np.greater(operating_cashflow, 0)
 
 
-
 def price_to_earnings_ratio(net_income, total_outstanding_shares, buy_share_price):
+    '''To calculate price-to-earnings ratio.'''
 
     if len(net_income) != len(total_outstanding_shares) or len(total_outstanding_shares) != len(buy_share_price):
         return 'Cant do calculation as arrays are of different lengths'
 
+    earnings = net_income * 0.9
 
-    earnings = np.multiply(net_income, 0.9)
-
-    eps_estimated = np.divide(earnings, total_outstanding_shares, out=np.zeros_like(earnings), where=total_outstanding_shares!=0)
+    eps_estimated = np.divide(earnings, total_outstanding_shares, out=np.zeros_like(
+        earnings), where=total_outstanding_shares != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
 
-    return np.divide( buy_share_price, eps_estimated, out=np.zeros_like(buy_share_price), where=eps_estimated!=0)
+    return np.divide(buy_share_price, eps_estimated, out=np.zeros_like(buy_share_price), where=eps_estimated != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
-
 
 
 def market_cap(buy_share_price, total_outstanding_shares):
+    '''To get market capitalization.'''
 
     if len(buy_share_price) != len(total_outstanding_shares):
         return 'Cant do calculation as arrays are of different lengths'
 
-    
-    return np.multiply(buy_share_price, total_outstanding_shares)
+    return buy_share_price * total_outstanding_shares
 
 
-
-# Conservative Net Asset Value (CNAV) per share
 def cnav(cash, ppe, total_liabilities, total_outstanding_shares):
-    
+    '''To calculate CNAV (Conservative Net Asset Value) per share.'''
+
     if len(cash) != len(ppe) or len(ppe) != len(total_liabilities) or len(total_liabilities) != len(total_outstanding_shares):
         return 'Cant do calculation as arrays are of different lengths'
 
-
     # I am assuming that (property investments + land) is 50% of PP&E since I don't have exact figures
-    ppe = np.multiply(ppe, 0.5)
-    net_assets = np.subtract( np.add(cash, ppe), total_liabilities)
+    ppe = ppe * 0.5
+    conservative_net_assets = (cash + ppe) - total_liabilities
 
-    return np.divide(net_assets, total_outstanding_shares, out=np.zeros_like(net_assets), where=total_outstanding_shares!=0)
+    return np.divide(conservative_net_assets, total_outstanding_shares, out=np.zeros_like(conservative_net_assets), where=total_outstanding_shares != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
 
 
-
-# Net Asset Value (NAV) per share
 def nav(total_assets, total_liabilities, total_outstanding_shares):
+    '''To calculate NAV (Net Asset Value) per share.'''
 
     if len(total_assets) != len(total_liabilities) or len(total_liabilities) != len(total_outstanding_shares):
         return 'Cant do calculation as arrays are of different lengths'
 
-    net_assets = np.subtract(total_assets, total_liabilities)
+    net_assets = total_assets - total_liabilities
 
-    return np.divide( net_assets, total_outstanding_shares, out=np.zeros_like(net_assets), where=total_outstanding_shares!=0 )
+    return np.divide(net_assets, total_outstanding_shares, out=np.zeros_like(net_assets), where=total_outstanding_shares != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
 
 
-
 def roi(cnav_share_price, nav_share_price):
+    '''To calculate ROI based on CNAV and NAV per share.'''
 
     # Check if arrays are of the same length
     if len(nav_share_price) != len(cnav_share_price):
         return 'Cant do calculation as arrays are of different lengths'
 
-    potential_gain = np.subtract(nav_share_price, cnav_share_price)
+    potential_gain = nav_share_price - cnav_share_price
 
-    return np.divide(potential_gain, cnav_share_price, out=np.zeros_like(potential_gain), where=cnav_share_price!=0)
+    return np.divide(potential_gain, cnav_share_price, out=np.zeros_like(potential_gain), where=cnav_share_price != 0)
     # Divides the calculation anywhere where b != zero. But when b = zero, then it remains unchanged from whatever value you originally gave it in the 'out' argument
--- Jin/Version_01/debug_matrix.py
+++ Jin/Version_01/debug_matrix.py
@@ -1,9 +1,7 @@
-
+from datetime import date
+import json
 import pandas as pd
 import numpy as np
-import json 
-from datetime import date
-from backtest_strategy.check_shareprices import *
 
 
 '''
@@ -12,33 +10,30 @@ matrix[2009-08-26][GOOG] --> 116.4467 close price
 matrix[2015-11-09][BLK] --> 349.57 close price
 '''
 
+
 def debug_converting_df_to_matrix(matrix_filename, dict_company_index_matrix_filename, selected_date, company, expected_value):
 
     # Open all the newly created and formatted data files
     matrix = pd.read_pickle(matrix_filename)
-    dict_company_index_matrix = json.load( (open(dict_company_index_matrix_filename)) )
-
+    dict_company_index_matrix = json.load(
+        (open(dict_company_index_matrix_filename)))
 
     # Get date index and company index, from Datetime date and company name (string)
-    date_index = ( selected_date - date(2007, 1, 3) ).days
+    date_index = (selected_date - date(2007, 1, 3)).days
 
     # Convert expected value into the same format as data in matrix
     expected_value = np.float32(expected_value)
 
-
     # Get value from matrix
-    company_index = dict_company_index_matrix[ company ]
+    company_index = dict_company_index_matrix[company]
     value_to_check = matrix[date_index][company_index]
 
     # If value_to_check equals to the condition (aka expected value), nothing happens. Else, return an AssertionError
     if value_to_check == expected_value:
         print('Debugging test checks out. The matrix was created correctly.')
-    
-    else: 
+
+    else:
         print('Something is wrong. The matrix was created incorrectly!')
-
-    
-
 
 
 '''
@@ -54,22 +49,20 @@ Company index of matrix: 1121
 Date index with shareprice gain: 2316
 '''
 
-# To get shareprice data from company name and date (instead of company index, date index)
+
 def get_shareprice_in_matrix(company_name, selected_date):
+    '''To get shareprice data from company name and date (instead of company index, date index).'''
 
     matrix = pd.read_pickle('matrix.pkl')
 
-    
     # Get date index from date input
     matrix_start_date = date(2007, 1, 3)
     date_index = (selected_date - matrix_start_date).days
- 
-
 
     # Get company index from company name input
-    dict_company_index_matrix = json.load( (open('dict_company_index_matrix.json')) )
+    dict_company_index_matrix = json.load(
+        (open('dict_company_index_matrix.json')))
     company_index = dict_company_index_matrix[company_name]
-
 
     shareprice = matrix[date_index][company_index]
     print(shareprice)
